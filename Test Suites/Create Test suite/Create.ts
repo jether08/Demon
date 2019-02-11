3869f34091e06f71a94a26eae51b1fbd66c17d89
arcillas <arcillas@arcillas-PC>
2019-02-11 11:07:22 +0800

    Create Test suites

---
+++ Test Suites/Create Test suite/Create.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Create</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ef09029f-7f43-4c0c-a346-ef26fb7d5720</testSuiteGuid>
+   <testCaseLink>
+      <guid>2c6e6962-d3ca-4b22-bee9-1d8ebabf6566</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create Client</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>94651535-0a9d-40f5-8bd6-3809ed55bc50</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create Sub Section</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
